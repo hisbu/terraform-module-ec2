c01b183f82edf3dab020c4bf3952789a0018dd2e
hisbu <lab.hisbu@gmail.com>
2021-02-05 11:18:10 +0700

    first init

---
+++ vars.tf
@@ -0,0 +1,35 @@
+variable "INSTANCE_COUNT" {
+  description = "jumlah instance yang akan di deploy"
+  type = number
+  default = 1
+}
+
+variable "AMI" {
+  description = "ami id"
+  type = string
+}
+
+variable "INSTANCE_TYPE" {
+  description = "tipe instance"
+  type = string
+}
+
+variable "SECURITY_GROUPS" {
+  description = "list of security group"
+  type = list
+}
+
+variable "KEYNAME" {
+  description = "key name"
+  type = string
+}
+
+variable "USERDATA" {
+  description = "user data untuk install apps"
+  type = string
+}
+
+variable "TAG_NAME" {
+  description = "tag name"
+  type = string
+}
